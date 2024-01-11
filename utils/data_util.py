ac99ca90af2932310207b1c53f5afc88a860d4db
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2024-01-11 19:01:32 +0100

    restructure project

--- utils/data_util.py
+++ utils/data_util.py
@@ -8,49 +8,6 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sqlalchemy import create_engine
 
-class HousingPriceDataset(Dataset):
-    """Housing price dataset."""
-
-    def __init__(self, csv_file, train=True):
-        """
-        Arguments:
-            csv_file (string): Path to the csv file with housing price data.
-        """
-        self.train = train
-        self.df = pd.read_csv('housing_price_dataset.csv')
-        # print(f'Missing values:\n{self.df.isna().sum()}')
-        self.ydf = self.df[['Price']]
-        self.Xdf = self.df.drop(columns=['Price'])
-        # print(f'\nShapes:\nX: {self.Xdf.shape}\ny: {self.ydf.shape}')
-        self.Xdf_tr, self.Xdf_te, self.ydf_tr, self.ydf_te = train_test_split(self.Xdf, self.ydf, test_size=0.1, random_state=42)
-        # print(f'Shapes:\nXtr: {self.Xdf_tr.shape}\nXte: {self.Xdf_te.shape}\nytr: {self.ydf_tr.shape}\nyte: {self.ydf_te.shape}')
-        self.preprocessor = ColumnTransformer(
-            [("Categorical", OneHotEncoder(), ['Bedrooms', 'Bathrooms', 'Neighborhood']),
-            ("Numerical", StandardScaler(), ['SquareFeet', 'YearBuilt'])]
-        )
-        self.target_scaler = StandardScaler()
-
-        self.Xtr = torch.Tensor(self.preprocessor.fit_transform(self.Xdf_tr))
-        self.Xte = torch.Tensor(self.preprocessor.transform(self.Xdf_te))
-        self.ytr = torch.Tensor(self.target_scaler.fit_transform(self.ydf_tr))
-        self.yte = torch.Tensor(self.target_scaler.transform(self.ydf_te))
-
-        # print(f'Shapes:\nXtr: {self.Xtr.shape}\nXte: {self.Xte.shape}\nytr: {self.ytr.shape}\nyte: {self.yte.shape}')
-
-    def __len__(self):
-        if self.train:
-            return len(self.Xtr)
-        else:
-            return len(self.Xte)
-
-    def __getitem__(self, idx):
-        if self.train:
-            return (self.Xtr[idx], self.ytr[idx])
-        else:
-            return (self.Xte[idx], self.yte[idx])
-        
-
-
 class HackerNewsBigrams(Dataset):
     """Hacker News dataset."""
 
--- utils/nn_util.py
+++ utils/nn_util.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -55,3 +57,68 @@ def test(dataloader, model, loss_fn, device):
             test_loss += loss_fn(pred, y).item()
     test_loss /= num_batches
     return test_loss
+
+
+class EmbeddingsNN:
+    def __init__(self, embedding_dims, context_size, batch_size, device):
+        self.batch_size = batch_size
+        self.embedding_dims = embedding_dims
+        self.context_size = context_size
+        self.device = device
+        self.g = torch.Generator(device=self.device).manual_seed(0)
+        self.C = torch.randn((28, self.embedding_dims), generator=self.g, device=self.device) * 0.01# embedding lookup-table
+        self.W1 = torch.randn((self.context_size*self.embedding_dims, 256), generator=self.g, device=self.device) * (2/(self.context_size*self.embedding_dims))**0.5 # weights hidden layer
+        self.b1 = torch.randn(256, generator=self.g, device=self.device) * 0.01 # biases hidden layer
+        self.W2 = torch.randn((256, 28), generator=self.g, device=self.device) * (2/256)**0.5 # weights output layer
+        self.b2 = torch.randn(28, generator=self.g, device=self.device) * 0.01 # biases output layer
+        self.parameters = [self.C, self.W1, self.b1, self.W2, self.b2]
+        for p in self.parameters:
+            p.requires_grad = True
+
+    def forward(self, X, y):
+        self.embedding = (X @ self.C).view(self.batch_size, self.context_size*self.embedding_dims)
+        self.h = torch.relu(self.embedding @ self.W1 + self.b1) # (batch_size, 256)
+        self.logits = self.h @ self.W2 + self.b2 # (batch_size, 28)
+        self.loss = F.cross_entropy(self.logits, y)
+        return self.loss.item()
+        
+    def backward(self):
+        for p in self.parameters:
+            p.grad = None
+        self.loss.backward()
+
+    def update_params(self, lr):
+        for p in self.parameters:
+            p.data += -lr * p.grad
+
+    def sample(self, ctoi, itoc):
+        text = ''
+        n = 0
+        nll = 0.0
+        context = ['<>'] * self.context_size
+
+        while True:
+            X = F.one_hot(torch.tensor([ctoi[c] for c in context], device=self.device), num_classes=28).float()
+            
+            # forward pass
+            with torch.no_grad():
+                self.embedding = (X @ self.C).view(self.context_size*self.embedding_dims)
+                self.h = torch.relu(self.embedding @ self.W1 + self.b1) 
+                self.logits = self.h @ self.W2 + self.b2 
+                self.probs = F.softmax(self.logits, dim=0)
+                ix = torch.multinomial(self.probs, num_samples=1, replacement=True, generator=self.g).item()
+
+
+            # break if end-character was sampled
+            if ix==ctoi['<>']:
+                break
+            
+            # update params
+            context = context[1:] + [itoc[ix]]
+            text += itoc[ix]
+            n += 1
+
+            # calculate negative log loss
+            p = self.probs[ix].log().item()
+            nll -= p
+        return text, nll/n
